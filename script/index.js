a655af4de1993d5d2b0d0d2dc22af6d835943090
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2022-11-16 11:05:44 +0700

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -57,3 +57,17 @@ fetch('https://dummyjson.com/quotes/random')
 
 const a = 'b' + 'a' + +'a' + 'a';
 console.log(a);
+
+function CopyToClipboard(containerid) {
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(containerid));
+    range.select().createTextRange();
+    document.execCommand('copy');
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(containerid));
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+  }
+}
